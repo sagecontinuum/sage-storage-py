fd5eaabb83df80b178637b3d8f386e9546478c6c
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-04-28 17:16:28 -0500

    added bucket operations

--- sage_storage/sage_storage.py
+++ sage_storage/sage_storage.py
@@ -3,9 +3,43 @@
 
 import requests
 import json
-
+import sys
 import logging
 
+
+
+def createHeader(token):
+    headers = {}
+    if token:
+        headers["Authorization"] = "sage "+token
+    return headers
+
+
+def doRequest(method, url, **kwargs):
+
+    logging.debug(method +" "+url)
+
+    try:
+        r = requests.request(method, url, **kwargs)
+    except:
+        raise
+
+    if r.status_code != 200:
+        try:
+            result = r.json()
+        except Exception:
+            raise Exception("status_code: {}".format(r.status_code))
+
+        
+        raise Exception("status_code: {} {}".format(r.status_code, result))
+
+    try:
+        #result = r.json()
+        json_data = json.loads(r.text)
+    except:
+        raise
+
+    return json_data
 
 #curl  -X POST 'localhost:8080/api/v1/objects?type=training-data&name=mybucket'  -H "Authorization: sage ${SAGE_USER_TOKEN}"
 def createBucket(host, token, name, datatype, debug=False):
@@ -13,10 +47,8 @@ def createBucket(host, token, name, datatype, debug=False):
     if not host :
         raise "host not defined"
 
-
-    headers = {}
-    if token:
-        headers["Authorization"] = "sage "+token
+    headers = createHeader(token)
+    
 
     params = {}
     
@@ -31,31 +63,60 @@ def createBucket(host, token, name, datatype, debug=False):
     if debug:
         logging.debug(url)
 
-    try:
-        r = requests.post(url, headers=headers , params=params)
-    except Exception as e:
-        raise
-
-    if r.status_code != 200:
-        try:
-            result = r.json()
-        except Exception as e:
-            raise Exception("status_code: {}".format(r.status_code))
-
-        
-        raise Exception("status_code: {} {}".format(r.status_code, result))
-
-    try:
-        #result = r.json()
-        json_data = json.loads(r.text)
-    except Exception as e:
-        raise
-
-    return json_data
-
+    return doRequest("POST", url, headers=headers, params=params)
+    
+def showBucket(host, token, bucketID, debug=False):
     
 
+    if not host :
+        raise "host not defined"
+
+    headers = createHeader(token)
+    
+   
+
+    url = str(host) + "/api/v1/objects/" + bucketID
+
+    
+    
+    
+    return doRequest("GET", url, headers=headers)
+
+
+# TODO add query
+def listBuckets(host, token, debug=False):
+    
+
+    if not host :
+        raise "host not defined"
+
+    headers = createHeader(token)
+    
+   
+
+    url = str(host) + "/api/v1/objects"
+
+
+    return doRequest("GET", url, headers=headers)
     
 
 
+
+
+def getPermissions(host, token, bucketID, debug=False):
+    
+
+    if not host :
+        raise "host not defined"
+
+    headers = createHeader(token)
+    
+   
+    params = {"permissions" : True}
+    url = str(host) + "/api/v1/objects/" + bucketID 
+
+    if debug:
+        logging.debug(url)
+
+    return doRequest("GET", url, headers=headers, params=params)
     
